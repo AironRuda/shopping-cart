2699031a7e06bbec36f34a47afb38c596706a86a
AironRuda <airon.ruda@udea.edu.co>
2023-02-06 11:42:20 -0500

    feat: store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,18 @@
+import storeItems from "../data/items.json";
+import { Row, Col } from "react-bootstrap";
+import StoreItem from "../components/StoreItem";
 const Store = () => {
-  return <div>Store</div>;
+  return (
+    <>
+      <Row lg={3} md={2} sm={1} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default Store;
